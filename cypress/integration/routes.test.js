978efe6cd3ed6acb1be1c78f5a7540e854b3af49
gabyballester <gaby.ballester@gmail.com>
2024-07-24 18:43:46 +0200

    Add cy tests for shows and bands routes

---
+++ cypress/integration/routes.test.js
@@ -0,0 +1,12 @@
+it("displays correct heading when navigating to shows route", () => {
+  cy.visit("/")
+  cy.findByRole("button", { name: /shows/i }).click();
+  cy.findByRole("heading", { name: /upcoming shows/i }).should("exist");
+});
+
+it("displays correct heading when navigating to bands route", () => {
+  cy.visit("/")
+  cy.findByRole("button", { name: /bands/i }).click();
+  cy.findByRole("heading", { name: /illustrious performers/i }).should("exist");
+});
+
