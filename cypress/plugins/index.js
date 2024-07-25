2f729b4c774f93a0c5d44ba375a2fecb3bfcd47e
gabyballester <gaby.ballester@gmail.com>
2024-07-25 11:17:08 +0200

    Add testing on reservations on interval

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -15,6 +15,7 @@
 
 const { resetDB } = require("../../__tests__/__mocks__/db/utils/reset-db");
 const { addBand } = require("../../lib/features/bands/queries");
+const { addReservation } = require("../../lib/features/reservations/queries");
 
 /**
  * @type {Cypress.PluginConfig}
@@ -29,6 +30,8 @@ module.exports = (on, config) => {
   on("task", {
     "db:reset": () => resetDB().then(() => null),
     addBand: (newBand) => addBand(newBand).then(() => null),
+    addReservation: (newReservation) =>
+      addReservation(newReservation).then(() => null),
   });
 
   return config;
