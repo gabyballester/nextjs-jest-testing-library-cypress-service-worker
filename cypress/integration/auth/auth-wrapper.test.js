87e2a1063524ef9d258bb09dda7706997e7dc52b
gabyballester <gaby.ballester@gmail.com>
2024-07-26 13:23:08 +0200

    Add cy tests to reservations page

---
+++ cypress/integration/auth/auth-wrapper.test.js
@@ -0,0 +1,97 @@
+describe("Auth flow tests for reservations page", () => {
+
+  it("runs auth flow for successful login to protected reservations page", () => {
+    // visit reservations page for the first show (id = 0)
+    cy.task("db:reset").visit("/reservations/0");
+
+    // check for sign in form
+    cy.findByRole("heading", { name: /Sign in to your account/i }).should(
+      "exist"
+    );
+
+    // check that there's no option to purchase tickets
+    cy.findByRole("button", { name: /purchase/i }).should("not.exist");
+
+    // enter valid sign-in credentials
+    cy.findByLabelText(/email address/i)
+      .clear()
+      .type(Cypress.env("TEST_USER_EMAIL"));
+
+    cy.findByLabelText(/password/i)
+      .clear()
+      .type(Cypress.env("TEST_PASSWORD"));
+
+    // submit the form
+    cy.findByRole("main").within(() => {
+      cy.findByRole("button", { name: /sign in/i }).click();
+    });
+
+    // check for purchase button and band name
+    cy.findByRole("button", { name: /purchase/i }).should("exist");
+    cy.findByRole("heading", { name: /the wandering bunnies/i });
+
+    // check for email and sign-out button on navbar
+    cy.findByRole("button", { name: Cypress.env("TEST_USER_EMAIL") }).should(
+      "exist"
+    );
+    cy.findByRole("button", { name: /sign out/i }).should("exist");
+
+    // check that sign in button does not exist
+    cy.findByRole("button", { name: /sign in/i }).should("not.exist");
+  });
+
+  it("runs auth flow for protected user page, including failed sign in", () => {
+    // visit user page
+    cy.task("db:reset").visit("/user");
+
+    // check there's no welcome message
+    cy.findByRole("heading", { name: /welcome/i }).should("not.exist");
+
+    // check for sign in form
+    cy.findByRole("heading", { name: /Sign in to your account/i }).should(
+      "exist"
+    );
+
+    // fill out the sign in form with environment variable values, but bad password
+    cy.findByLabelText(/email address/i)
+      .clear()
+      .type(Cypress.env("TEST_USER_EMAIL"));
+
+    cy.findByLabelText(/password/i)
+      .clear()
+      .type("not my password");
+
+    // submit form (be sure to choose "sign in" button in form, not on nav!)
+    cy.findByRole("main").within(() =>
+      cy.findByRole("button", { name: /sign in/i }).click()
+    );
+
+    // check for failure message
+    cy.findByText(/sign in failed/i).should("exist");
+
+    // fill out the sign in form again, with correct info
+    cy.findByLabelText(/email address/i)
+      .clear()
+      .type(Cypress.env("TEST_USER_EMAIL"));
+
+    cy.findByLabelText(/password/i)
+      .clear()
+      .type(Cypress.env("TEST_PASSWORD"));
+
+    // submit form (be sure to choose "sign in" button in form, not on nav!)
+    cy.findByRole("main").within(() =>
+      cy.findByRole("button", { name: /sign in/i }).click()
+    );
+
+    // check that the user page now shows
+    cy.findByRole("heading", { name: /welcome/i }).should("exist");
+    cy.findByRole("heading", { name: /your tickets/i }).should("exist");
+
+    // check for user and sign out buttons on nav bar
+    cy.findByRole("button", { name: Cypress.env("TEST_USER_EMAIL") }).should(
+      "exist"
+    );
+    cy.findByRole("button", { name: /sign out/i }).should("exist");
+    cy.findByRole("button", { name: /sign in/i }).should("not.exist");
+  });
+})
